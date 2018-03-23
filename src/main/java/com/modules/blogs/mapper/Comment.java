554f3d92adb325bd020215e4125e58689a34d949
jiangyincai <438837583@qq.com>
2018-03-23 17:05:23 +0800

    数据库备份

---
+++ src/main/java/com/modules/blogs/mapper/Comment.java
@@ -0,0 +1,12 @@
+package com.modules.blogs.mapper;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: JIANGYINCAI
+ * Date: 2018/3/23
+ * Time: 15:57
+ * To change this template use File | Settings | File Templates.
+ * Description:评论
+ */
+public class Comment {
+}
---
+++ src/main/java/com/modules/blogs/mapper/Reply.java
@@ -0,0 +1,13 @@
+package com.modules.blogs.mapper;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: JIANGYINCAI
+ * Date: 2018/3/23
+ * Time: 15:58
+ * To change this template use File | Settings | File Templates.
+ * Description: 回复 评论
+ */
+public class Reply {
+
+}
